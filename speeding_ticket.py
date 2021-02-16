a641fcb578bb0561b9f82c50ad2c35dd0f62082c
ChrisLiangBDSC <78829865+ChrisLiangBDSC@users.noreply.github.com>
2021-02-16 20:03:52 +1300

    Add files via upload
    Speeding tick program 0.2! now featuring the use of 2D List and understanding of indexes!

--- speeding_ticket.py
+++ speeding_ticket.py
@@ -1,49 +1,112 @@
 speed = 0
 speed_limit = 0
 wanted_list = ["Jones", "Eric", "Dylan"]
+fines = []
+summary_list = []
+over_limit = 0
+statement = ""
+user_answer = ""
 
-import os
+def user_inputs():
+    global speed
+    global speed_limit
+    while True:
+        try:
+            speed = int(input("\nWhat was the speed you were going at? : "))
             
-def calculate_fine(x, y):   
+            if speed <= 0:
+                print("Please enter proper numbers\n")
+
+            else:
+                while True:
+                    try:
+                        speed_limit = int(input("\nWhat was the speed limit? : "))
+
+                        if speed_limit <= 0:
+                            print("Please enter proper numbers\n")
+                        else:
+                            break
+                    except ValueError:
+                        print("Please enter valid intergers\n")
+                break
+
+        except ValueError:
+            print("Please enter valid intergers\n")
+
+def calculate_fine(x, y):
+    global over_limit
     over_limit = x - y
-    return over_limit
-
-def speeding_summary(x):
-    if x > 0 and x < 50:
-        print(name, "You are over the speeding limit you must pay a fine of {:.2f}".format(x * 10))
-    elif x >= 50 and x <= 80:
-        print(name, "You are well over the speeding limit you must hand in your liscence")
-    elif x > 80:
-        print(name, "You are well beyond the speeding limit you must spend time in jail")
-    else:
-        print(name, "You are below the speeding limit you do not have to pay or do anything!")    
     
-name = input("Please enter your name: ")
 
-for x in range(0, len(wanted_list)):
-    if name.lower().strip() == wanted_list[x].lower():
-        print("{}, you are wanted by the police".format(name.strip().lower().capitalize()))
+def fine_checker(x):
+    global statement
+
+    if x > 0 and x < 50:
+        statement = "The speed of your Vechile going at was {}kms^1 and the Limit was {}; \nYou are {}km^-1 OVER the speeding limit you MUST pay a fine of {:.2f}".format(x * 10)
+    elif x >= 50 and x <= 80:
+        statement = "The speed of your Vechile going at was {}kms^1 and the Limit was {}; \nYou are {}km^-1 OVER the speeding limit you MUST hand in your liscence".format(speed, speed_limit ,over_limit)
+    elif x > 80:
+        statement = "The speed of your Vechile going at was {}kms^1 and the Limit was {}; \nYou are {}km^-1 WELL beyond the speeding limit you MUST spend time in jail".format(speed, speed_limit ,over_limit)
+    else:
+        statement = "The speed of your Vechile going at was {}kms^1 and the Limit was {}; \nYou are {}km^-1 BELOW the speeding limit you do not have to pay or do anything!".format(speed, speed_limit ,over_limit * -1)
+
+    print(statement)
+
+def summary_manager():
+    global summary_list
+
+    summary_list.append(["\nName: {}\n".format(name.lower().capitalize()),"Your speed: {}\n".format(speed), "The speed limit: {}".format(speed_limit),"\nSummary: {}".format(statement)])
+
+    user_answer = input("\nWould you like to take a look at your list of records? (Y/N): ").strip().lower()
+    while True:
+        if user_answer == "y":
+            while True:
+                try:
+                    user_answer = int(input("\nWhich record would you like to read?\nPlease enter from 1-{} for corrisponding record : ".format(len(summary_list))))
+
+                    if user_answer <= len(summary_list) and user_answer >= 1:
+                        print(''.join(summary_list[user_answer - 1]))
+                        break
+                    
+                    elif user_answer <= 0 or isinstance(user_answer, float):
+                        print("\nPlease enter valid whole numbers")
+                except ValueError:
+                    print("\nPlease enter valid numbers")
+        elif user_answer == "n":
+            break
+        else:
+            print("Please answer valid input Y or N")
+        break
+    
+
+
+
 
 while True:
-    try:
-        speed = int(input("What is the speed that you were going: "))
-        if speed < 0:
-            print("Please enter a valid interger")
-        else:
-            break
-    except ValueError:
-        print("Please enter an interger")
+    name = input("Please enter your name: ")
+    for i in range(0, len(wanted_list)):
+        if name.upper().capitalize() == wanted_list[i]:
+            print("{} is wanted for arrest".format(name.lower().capitalize()))
+    
+    user_inputs()
+    calculate_fine(speed, speed_limit)
+    fine_checker(over_limit)
+    summary_manager()
+
+
+    user_answer = input("\nWould you like to enter another record? (Y/N): ").lower().strip()
+
+    if user_answer == "y":
+        continue
+    elif user_answer == "n":
+        break
+    else:
+        print("Please enter Y or N")
+
+    
+
+    
+    
+
+
         
-while True:
-    try:
-        speed_limit = int(input("What is the speed limit?: ")) 
-        if speed_limit < 0:
-            print("Please enter a valid interger")
-        else:
-            break
-    except ValueError:
-        print("Please enter an interger")
-
-over_limit = calculate_fine(speed, speed_limit)
-over_limit = calculate_fine(speed, speed_limit)
-summary = speeding_summary(over_limit)
